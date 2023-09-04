7103c8ac8dac6327e8d9739ffba88231ed727fa5
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2023-09-04 17:08:28 +0200

    Init get api

--- django_comparison_dashboard/api.py
+++ django_comparison_dashboard/api.py
@@ -3,6 +3,6 @@ from ninja import NinjaAPI
 api = NinjaAPI()
 
 
-@api.get("/add")
-def add(request, a: int, b: int):
-    return {"result": a + b}
+@api.get("/hello")
+def hello(request):
+    return "Hello world"
