338420f468cc64226d0f13e9edb6fb2e63ce561f
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2023-09-04 17:16:33 +0200

    Remove admin urls from api

--- django_comparison_dashboard/urls.py
+++ django_comparison_dashboard/urls.py
@@ -1,4 +1,3 @@
-from django.contrib import admin
 from django.urls import path
 
 from .api import api
@@ -7,6 +6,5 @@ app_name = "django_comparison_dashboard"
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("api/", api.urls),
 ]
