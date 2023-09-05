152a6b8e49054155e9b1a86310b731bcb5ea9298
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2023-09-05 15:52:07 +0200

    Add models.py for data models

---
+++ django_comparison_dashboard/models.py
@@ -0,0 +1,45 @@
+from django.contrib.postgres.fields import ArrayField
+from django.db import models
+
+
+class Source(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+
+
+class Scenario(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    source = models.ForeignKey(Source, on_delete=models.CASCADE, related_name="scenarios")
+
+
+class Data(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    region = models.CharField(max_length=255)
+    input_energy_vector = models.CharField(max_length=255)
+    output_energy_vector = models.CharField(max_length=255)
+    parameter_name = models.CharField(max_length=255)
+    technology = models.CharField(max_length=255)
+    technology_type = models.CharField(max_length=255)
+    unit = models.CharField(max_length=255)
+    tags = models.JSONField(max_length=255)
+    method = models.JSONField(max_length=255)
+    source = models.CharField(max_length=255)
+    comment = models.CharField(max_length=255)
+
+    class Meta:
+        abstract = True
+
+
+class ScalarData(Data):
+    scenario = models.ForeignKey(Scenario, on_delete=models.CASCADE, related_name="scalars")
+    value = models.FloatField()
+    year = models.IntegerField()
+
+
+class TimeseriesData(Data):
+    scenario = models.ForeignKey(Scenario, on_delete=models.CASCADE, related_name="timeseries")
+    timeindex_start = models.TimeField()
+    timeindex_stop = models.TimeField()
+    timeindex_resolution = models.DurationField()
+    series = ArrayField(models.FloatField())
