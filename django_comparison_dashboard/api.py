fbaf17514f196a0bb77999672fe596556cba954d
josihoppe <116898820+josihoppe@users.noreply.github.com>
2023-09-06 11:53:21 +0200

    trying to add index view

--- django_comparison_dashboard/api.py
+++ django_comparison_dashboard/api.py
@@ -1,12 +1,17 @@
-from ninja import NinjaAPI
+from ninja import NinjaAPI, Router
 from django.http import HttpResponse
+from django.shortcuts import render
 
 api = NinjaAPI()
-
+router = Router()
 
 @api.get("/hello")
 def hello(request):
     return "Hello world htmx"
+
+@router.get("/index")
+def index(request):
+    return render(request, "index.html")
 
 @api.get("/htmx-magic-happens")
 def magic(request):
