95e5e35972b004f667d1a36d2771ebdc499c6000
josihoppe <116898820+josihoppe@users.noreply.github.com>
2023-09-06 17:08:29 +0200

    trying templateView

--- django_comparison_dashboard/urls.py
+++ django_comparison_dashboard/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from django_comparison_dashboard import views
 from .api import api
 
 app_name = "django_comparison_dashboard"
@@ -7,4 +8,5 @@ app_name = "django_comparison_dashboard"
 
 urlpatterns = [
     path("api/", api.urls),
+    path('index/', views.IndexView.as_view(), name='index'),
 ]
