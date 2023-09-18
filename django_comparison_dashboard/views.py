c966aaaac3e97833691caeb2ddf8a9687990150b
josihoppe <116898820+josihoppe@users.noreply.github.com>
2023-09-18 15:36:36 +0200

    changed structure, made it work

--- django_comparison_dashboard/views.py
+++ django_comparison_dashboard/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
-    template_name = "button.html"
+    template_name = "django_comparison_dashboard/button.html"
 
 def magic(request):
     return HttpResponse("Ta! Daaa!")
