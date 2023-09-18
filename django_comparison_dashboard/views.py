b56157c7727556f536f548da8287e9f4b4a49c10
josihoppe <116898820+josihoppe@users.noreply.github.com>
2023-09-18 11:13:44 +0200

    trying different url naming

--- django_comparison_dashboard/views.py
+++ django_comparison_dashboard/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
-    template_name = "index.html"
+    template_name = "button.html"
 
 def magic(request):
     return HttpResponse("Ta! Daaa!")
