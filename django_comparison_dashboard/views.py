0842f8fcff3be087601f28b0e3fa3996c2d3045e
josihoppe <116898820+josihoppe@users.noreply.github.com>
2023-09-27 11:49:24 +0200

    choosing a scenario changes options in filter

--- django_comparison_dashboard/views.py
+++ django_comparison_dashboard/views.py
@@ -1,8 +1,21 @@
-from django.http import HttpResponse
+import json
+from django.http import HttpResponse, JsonResponse
 from django.views.generic import TemplateView
+
 
 class IndexView(TemplateView):
     template_name = "django_comparison_dashboard/dashboard.html"
 
 def load_filter(request):
-    return HttpResponse("Ta! Daaa!")
+    scenario = request.GET.get('selected_item')
+    #with open('../data/django_comparison_dashboard/dataset.json') as file:
+    #    data = json.load(file)
+    
+    filter_options = ["ID1", "ID2", "ID9"]
+    response = ""
+    for filter in filter_options:
+        print(filter)
+        option = "<option value=\" " + filter + " \">" + filter + "</option>"
+        response += option + " "
+
+    return HttpResponse(response)
