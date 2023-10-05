a87e07565d7b22f1a43ac7ed594a7c262d39b291
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2023-10-05 14:17:02 +0200

    Change partials to inline partials; wrap filter form with partials

--- django_comparison_dashboard/views.py
+++ django_comparison_dashboard/views.py
@@ -1,5 +1,3 @@
-import json
-
 from django.shortcuts import render
 
 from django_comparison_dashboard.forms import Scenario
@@ -7,14 +5,20 @@ from django_comparison_dashboard.forms import Scenario
 
 def index(request):
     filter_options = []
-    if request.method == 'POST':
+    if request.method == "POST":
         form = Scenario(request.POST)
         if form.is_valid():
-            filter_options = ["ID1", "ID2", "ID9"] # TODO get the options from data with form value
-            context = {'filter_options': filter_options}
-            return render (request, 'django_comparison_dashboard/dashboard.html#filter-option', context)
+            filter_options = [
+                "ID1",
+                "ID2",
+                "ID9",
+            ]  # TODO get the options from data with form value
+            context = {"filter_options": filter_options}
+            return render(
+                request, "django_comparison_dashboard/dashboard.html#filters", context
+            )
         else:
-            print(form.errors)  
+            print(form.errors)
 
-    context = {'scenario_form': Scenario(), 'filter_options': filter_options}
-    return render(request, 'django_comparison_dashboard/dashboard.html', context)
+    context = {"scenario_form": Scenario(), "filter_options": filter_options}
+    return render(request, "django_comparison_dashboard/dashboard.html", context)
