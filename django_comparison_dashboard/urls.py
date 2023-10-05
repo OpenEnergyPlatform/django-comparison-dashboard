10a0613a38f9a45d5d86101fd98664f54c3e9f9e
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2023-10-05 14:22:09 +0200

    Move filter API into separate view

--- django_comparison_dashboard/urls.py
+++ django_comparison_dashboard/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 
 from django_comparison_dashboard import views
 
-
 app_name = "django_comparison_dashboard"
 
 urlpatterns = [
-    path('index/', views.index, name='index'),
+    path("index/", views.index, name="index"),
+    path("filters/", views.get_filters, name="filters"),
 ]
--- django_comparison_dashboard/views.py
+++ django_comparison_dashboard/views.py
@@ -4,21 +4,21 @@ from django_comparison_dashboard.forms import Scenario
 
 
 def index(request):
-    filter_options = []
-    if request.method == "POST":
-        form = Scenario(request.POST)
-        if form.is_valid():
-            filter_options = [
-                "ID1",
-                "ID2",
-                "ID9",
-            ]  # TODO get the options from data with form value
-            context = {"filter_options": filter_options}
-            return render(
-                request, "django_comparison_dashboard/dashboard.html#filters", context
-            )
-        else:
-            print(form.errors)
-
-    context = {"scenario_form": Scenario(), "filter_options": filter_options}
+    context = {"scenario_form": Scenario(), "filter_options": []}
     return render(request, "django_comparison_dashboard/dashboard.html", context)
+
+
+def get_filters(request):
+    form = Scenario(request.POST)
+    if form.is_valid():
+        filter_options = [
+            "ID1",
+            "ID2",
+            "ID9",
+        ]  # TODO get the options from data with form value
+        context = {"filter_options": filter_options}
+        return render(
+            request, "django_comparison_dashboard/dashboard.html#filters", context
+        )
+    else:
+        print(form.errors)
