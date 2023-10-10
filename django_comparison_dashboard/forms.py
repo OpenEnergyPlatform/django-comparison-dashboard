f7d843a05bec72b50e98ea7db773b233853baa52
josihoppe <116898820+josihoppe@users.noreply.github.com>
2023-10-10 16:40:00 +0200

    added Filter as django Form

--- django_comparison_dashboard/forms.py
+++ django_comparison_dashboard/forms.py
@@ -10,3 +10,16 @@ class Scenario(forms.Form):
             ("ID1,2_paper", "ID1,2_paper"),
         ]
     )
+
+
+class Filter(forms.Form):
+    tags = forms.ChoiceField(label="Filter Tags")
+    year = forms.ChoiceField(label="Filter Year")
+    region = forms.ChoiceField(label="Filter Region")
+
+    def __init__(self, *args, **kwargs):
+        options = kwargs.pop("options", None)
+        super().__init__(*args, **kwargs)
+        if options:
+            for filter, choice_list in options.items():
+                self.fields[filter].choices = choice_list
--- django_comparison_dashboard/views.py
+++ django_comparison_dashboard/views.py
@@ -1,24 +1,35 @@
 from django.shortcuts import render
 
 from . import settings
-from .forms import Scenario
+from .forms import Filter, Scenario
 
 
 def index(request):
-    context = {"scenario_form": Scenario(), "filter_options": []}
+    filter_form = Filter(options=[])
+    context = {"scenario_form": Scenario(), "filter_form": filter_form}
     return render(request, "django_comparison_dashboard/dashboard.html", context)
 
 
 def get_filters(request):
     form = Scenario(request.POST)
     if form.is_valid():
-        filter_options = set()
+        tags_options = set()
+        year_options = set()
+        region_options = set()
         for scenario in form.cleaned_data["scenarios"]:
-            filter_options.update(settings.DATASET[scenario]["Tags"])
+            tags_options.update(settings.DATASET[scenario]["Tags"])
+            year_options.update(settings.DATASET[scenario]["Year"])
+            region_options.update(settings.DATASET[scenario]["Region"])
+        options = {
+            "tags": [(item, item) for item in tags_options],
+            "year": [(item, item) for item in year_options],
+            "region": [(item, item) for item in region_options],
+        }
+        filter_form = Filter(options=options)
         return render(
             request,
             "django_comparison_dashboard/dashboard.html#filters",
-            {"filter_options": filter_options},
+            {"filter_form": filter_form},
         )
     else:
         print(form.errors)
