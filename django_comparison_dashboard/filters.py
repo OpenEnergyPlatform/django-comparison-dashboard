b63664edb4e0badd17b769b459c97766c81a1df3
josihoppe <116898820+josihoppe@users.noreply.github.com>
2024-01-04 18:33:35 +0100

    added style to form components

--- django_comparison_dashboard/filters.py
+++ django_comparison_dashboard/filters.py
@@ -1,4 +1,5 @@
 import django_filters
+from django import forms
 
 from .models import ScalarData
 
@@ -10,7 +11,9 @@ class ScenarioFilter(django_filters.FilterSet):
         for field in ScalarData.filters:
             qs = self.queryset.order_by().values_list(field, flat=True).distinct()
             field_instance = django_filters.MultipleChoiceFilter(
-                field_name=field, choices=[(choice, choice) for choice in qs.all()]
+                field_name=field,
+                choices=[(choice, choice) for choice in qs.all()],
+                widget=forms.SelectMultiple(attrs={"class": "form-control"}),
             )
             self.filters[field] = field_instance
 
