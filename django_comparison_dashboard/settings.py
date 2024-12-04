56436a87376d80e8abf223fb635b3767c64611f9
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2024-12-04 09:55:21 +0100

    Bump version: 2.5.1 → 2.6.0

--- django_comparison_dashboard/settings.py
+++ django_comparison_dashboard/settings.py
@@ -3,7 +3,7 @@ import os
 import pathlib
 from enum import IntEnum
 
-VERSION = "2.5.1"
+VERSION = "2.6.0"
 
 USE_DUMMY_DATA = os.environ.get("USE_DUMMY_DATA", "False") == "True"
 SKIP_TS = os.environ.get("SKIP_TS", "False") == "True"
